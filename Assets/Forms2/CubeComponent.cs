4a55bc7633f4a771e139fd4afe7e0f4abdb323b3
Bryan Duggan <skooter500@gmail.com>
2022-11-01 12:24:15 +0000

    compiles

--- Assets/Forms2/CubeComponent.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Unity.Entities;
-
-// ReSharper disable once InconsistentNaming
-[GenerateAuthoringComponent]
-public struct CubeComponent : IComponentData
-{
-    public float RadiansPerSecond;
-}
